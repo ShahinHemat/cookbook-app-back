1aab9c4530b1c3de39a7787317376039fa5fff0f
Shahin Endregard Hemat <120106335+ShahinHemat@users.noreply.github.com>
2023-02-28 15:47:47 +0100

    Database progress

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 
-app.use(express.urlencoded({
- extended: true
-}));
+
+// Middleware
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+// This gives us access to the request.body object (data from from the client side)
 app.use(express.json());
+
+
 app.listen(3333, () => console.log("Listening on port 3333"));
